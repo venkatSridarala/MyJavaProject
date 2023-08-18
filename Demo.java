07eaa09cc6b4195aeac5786c40e0e049a031d0ee
venkat <venkatsridarala6@gmail.com>
2023-08-18 14:52:18 +0530

    MyJavaProject

---
+++ Demo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class A
+{
+	public static void main(String...args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = sc.nextInt();
+		for(i=0;i<=n;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
+
